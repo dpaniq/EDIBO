93fd39ad1049ffae99de3c91ab1f9c20824a8b01
Bogdan Gromov <42778511+dpaniq@users.noreply.github.com>
2020-07-08 15:22:57 +0300

    Update bashFunction.sh

--- HWs/HW5/bashFunction.sh
+++ HWs/HW5/bashFunction.sh
@@ -1,4 +1,5 @@
 # binary.sh
+# https://stackoverflow.com/questions/10822790/can-i-call-a-function-of-a-shell-script-from-another-shell-script
 function convert () # (Val Base)
 {
    val=$1
@@ -9,6 +10,16 @@ function convert () # (Val Base)
         val=$(( $val / $base ))
    done
    echo -n $result
+}
+
+# or
+# binary2.sh
+# https://unix.stackexchange.com/questions/223338/convert-a-value-into-a-binary-number-in-a-shell-script
+toBinary(){
+    local n bit
+    for (( n=$1 ; n>0 ; n >>= 1 ));
+    do  bit="$(( n&1 ))$bit"; done
+    printf "%s\n" "$bit"
 }
 
 # terminal
